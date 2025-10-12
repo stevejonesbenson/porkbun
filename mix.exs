adbd4312312e4b43ec31b7eb871bd6aa2e9e12bd
Stevejones Benson <bensonstevejones@gmail.com>
2025-10-13 03:22:39 +1000

    back to 0.1.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Porkbun.MixProject do
     [
       name: "Porkbun",
       description: "Elixir client for the Porkbun API",
-      version: "0.1.2",
+      version: "0.1.0",
       type: :library,
       visibility: :public,
       start_permanent: Mix.env() == :prod,
